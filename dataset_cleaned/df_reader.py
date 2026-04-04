85c73180b5982616c661429ebec12fdc75b7de28
KousikGvs <kousikgvs@gmail.com>
2026-04-04 22:35:45 +0530

    Fixed the Path Issue where files weren't able to acces the Path of the Data Frame

---
+++ dataset_cleaned/df_reader.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_new = pd.read_csv("dataset_cleaned\\output.csv")
+
+print(df_new.head())
--- src/data/loader.py
+++ src/data/loader.py
@@ -5,12 +5,9 @@ import pandas as pd
 import difflib
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from dataset.df_reader import df1, df2
 
 # loading the data from a CSV file to pandas data
-df1 = pd.read_csv("C:\\Users\\kousi\\Documents\\recommendation-system\\dataset\\tmdb_5000_credits.csv")
-
-# loading the data from a CSV file to pandas data
-df2 = pd.read_csv("C:\\Users\\kousi\\Documents\\recommendation-system\\dataset\\tmdb_5000_movies.csv")
 
 df = df1.merge(df2 , on="title" , how='inner')
 

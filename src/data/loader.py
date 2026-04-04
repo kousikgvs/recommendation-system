9a0d2af0ff4e71036c0bd2032817ff03637611d7
KousikGvs <kousikgvs@gmail.com>
2026-04-04 22:21:52 +0530

    Completed the Loader Part

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,1 +1,19 @@
 # Loader code comes Here
+
+import numpy as np
+import pandas as pd
+import difflib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# loading the data from a CSV file to pandas data
+df1 = pd.read_csv("C:\\Users\\kousi\\Documents\\recommendation-system\\dataset\\tmdb_5000_credits.csv")
+
+# loading the data from a CSV file to pandas data
+df2 = pd.read_csv("C:\\Users\\kousi\\Documents\\recommendation-system\\dataset\\tmdb_5000_movies.csv")
+
+df = df1.merge(df2 , on="title" , how='inner')
+
+selected_features = ['movie_id' , 'title','overview' , 'genres','keywords','cast','crew']
+
+

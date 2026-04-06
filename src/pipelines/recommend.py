b1753a8011618c3a6888cd51d5e63a3b0dc43098
KousikGvs <kousikgvs@gmail.com>
2026-04-06 22:48:53 +0530

    working on Recommending Movies python file

--- src/pipelines/recommend.py
+++ src/pipelines/recommend.py
@@ -1,1 +1,20 @@
 # Recommendation pipeline code comes Here
+
+# Preprocessing code comes Here
+import numpy as np
+import pandas as pd
+import difflib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import json
+from dataset_cleaned.df_reader import df_new
+
+# # Converting all the Text Data to Numbering vectors
+# vectorizer = TfidfVectorizer()
+# feature_vectors = vectorizer.fit_transform(df_new['clean_title'] + ' ' + df_new['tags'])
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=5000 , stop_words='english')
+feature_vectors = cv.fit_transform(df_new['tags'])
+feature_vectors.shape
+
